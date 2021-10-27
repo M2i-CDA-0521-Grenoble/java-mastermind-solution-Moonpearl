21951d7606d283b15049d3e31c749c59a02d4717
Moonpearl <romain.tello@gmail.com>
2021-10-27 14:13:56 +0200

    Add user input and input validation

--- src/App.java
+++ src/App.java
@@ -1,11 +1,43 @@
+import java.util.Scanner;
+
 public class App
 {
-    // Cette méthode est appelée automatiquement au lancement de l'application
+    /**
+     * Processus principal de l'application
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
         // Efface la console
         System.out.print("\033[H\033[2J");   
         System.out.flush();
         
-        System.out.print("Hello, world!");
+        // Crée une interface permettant de surveiller les saisies utilisateur dans la console
+        Scanner scanner = new Scanner(System.in);
+        // Attend une saisie utilisateur
+        String userInput = scanner.nextLine();
+        // Vérifie que la saisie utilisateur contient bien 4 caractères parmi les 6 couleurs existantes
+        boolean valid = true;
+        if (userInput.length() != 4) {
+            valid = false;
+        } else {
+            for (int i = 0; i < 4; i++) {
+                if (userInput.charAt(i) != 'R'
+                    && userInput.charAt(i) != 'V'
+                    && userInput.charAt(i) != 'B'
+                    && userInput.charAt(i) != 'J'
+                    && userInput.charAt(i) != 'C'
+                    && userInput.charAt(i) != 'M'
+                ) {
+                    valid = false;
+                    break;
+                }
+            }
+        }
+        if (valid) {
+            System.out.println("Saisie correcte.");
+        } else {
+            System.out.println("Saisie incorrecte.");
+        }
     }
 }
