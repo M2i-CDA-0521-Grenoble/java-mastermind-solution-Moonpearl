aefc4138f08d558c22a63173c4d05d2449c4922b
Moonpearl <romain.tello@gmail.com>
2021-10-27 15:05:57 +0200

    Add correct colors count

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+import java.nio.channels.FileChannel.MapMode;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -13,6 +14,8 @@ public class App
         // Efface la console
         System.out.print("\033[H\033[2J");   
         System.out.flush();
+
+        char[] solution = new char[] { 'M', 'M', 'C', 'R' };
         
         // Crée une interface permettant de surveiller les saisies utilisateur dans la console
         Scanner scanner = new Scanner(System.in);
@@ -22,7 +25,22 @@ public class App
         Pattern pattern = Pattern.compile("^[RVBJCM]{4}$");
         Matcher matcher = pattern.matcher(userInput);
         if (matcher.matches()) {
-            System.out.println("Saisie correcte.");
+
+            // Transforme la saisie utilisateur en tableau de caractères pour faciliter son exploitation
+            char[] proposition = userInput.toCharArray();
+            // Détermine les couleurs bien placées
+            // Initialise le compte des couleurs bien placées à zéro
+            int correctCount = 0;
+            // Pour chaque caractère de la proposition utilisateur
+            for (int i = 0; i < 4; i++) {
+                // Si ce caractère est le même que celui corrrespondant dans la solution
+                if (proposition[i] == solution[i]) {
+                    // Augmente le compte des couleurs bien placées de 1
+                    correctCount += 1;
+                }
+            }
+            System.out.println(correctCount);
+
         } else {
             System.out.println("Saisie incorrecte.");
         }
